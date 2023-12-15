a08bbd3340f37c0acf97bd5c450aed36a6a37114
Sean Wallace <sean.wallace.australia@gmail.com>
2023-12-15 14:57:57 +1100

    load up blog pages

---
+++ QUICKCHANGES.md
@@ -0,0 +1,10 @@
+ # Quick notes of changes to the generated code
+
+```sh
+notes of this install are in /Users/seanus/syduni/GitHubBible.docx section 4a
+```
+
+## 🚀 Coding changes
+ add PORT to consts.ts, astro.config.mjs, /pages/index.astro
+
+ render json files....
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import { PORT } from '/src/consts';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	server: {
+    // this is the astro3 default port anyway
+    port: PORT
+  },
 });
